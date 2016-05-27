ed5d01da44ced5d15f81c3ff7bd3a97854d993ce
crazymouse <crazymousethief@outlook.com>
2016-05-28 01:45:35 +0800

    Modifiy Segment class(add sum method and a new constructor).

--- src/pers/crazymouse/exercises/myclass/Segment.java
+++ src/pers/crazymouse/exercises/myclass/Segment.java
@@ -6,20 +6,27 @@ import pers.crazymouse.exercises.myinterface.GeometricContain;
  * Created by crazymouse on 5/27/16.
  */
 public class Segment extends Line implements GeometricContain<Point> {
+    private double length;
+
     public Segment() {
-        super(0, 0, 0, 0);
+        this(0, 0, 0, 0);
+    }
+
+    public Segment(double length) {
+        this(0, 0, length, 0);
     }
 
     public Segment(double x1, double y1, double x2, double y2) {
-        super(x1, y1, x2, y2);
+        this(new Point(x1, y1), new Point(x2, y2));
     }
 
     public Segment(Point begin, Point end) {
         super(begin, end);
+        length = begin.getDistance(end);
     }
 
     public double getLength() {
-        return begin.getDistance(end);
+        return length;
     }
 
     public boolean contains(Point t) {
@@ -32,6 +39,14 @@ public class Segment extends Line implements GeometricContain<Point> {
         } else {
             return a.begin.equals(b.begin) || a.begin.equals(b.end) || a.end.equals(b.begin) || a.end.equals(b.end);
         }
+    }
+
+    public static double sum(Segment... list) {
+        double sum = 0;
+        for (Segment i : list) {
+            sum += i.getLength();
+        }
+        return sum;
     }
 
     private boolean isOnInterval(double l, double r, double x) {
