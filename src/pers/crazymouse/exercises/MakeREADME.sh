ab89078d088dcd0a7684ec6ab60344260670b37a
crazymouse <crazymousethief@outlook.com>
2016-06-03 16:55:36 +0800

    Update something about README.

--- src/pers/crazymouse/exercises/MakeREADME.sh
+++ src/pers/crazymouse/exercises/MakeREADME.sh
@@ -1,14 +1,22 @@
+#!/usr/bin/env bash
+head="1. [x]"
 (echo "# [Java Exercises](http://crazy-mouse.cn/Codes/Java/UniversityCourse/src/pers/crazymouse/exercises/README.html)"
 echo ""
-echo "## Public Class"
+echo "## myclass"
 ls myclass/*.java| cut -d . -f 1 | while read i
 do
-        echo "1. * [x] ["${i##*/}"](http://crazy-mouse.cn/Codes/Java/UniversityCourse/src/pers/crazymouse/exercises/"$i".java)"
+        echo $head "["${i##*/}"](http://crazy-mouse.cn/Codes/Java/UniversityCourse/src/pers/crazymouse/exercises/"$i".java)"
 done
 echo ""
-echo "## Exercises"
+echo "## myinterface"
+ls myinterface/*.java| cut -d . -f 1 | while read i
+do
+        echo $head "["${i##*/}"](http://crazy-mouse.cn/Codes/Java/UniversityCourse/src/pers/crazymouse/exercises/"$i".java)"
+done
+echo ""
+echo "## exercises"
 ls *.java| cut -d . -f 1 | sort -n -t "_" -k 1.3 -k 2 | while read i
 do
-        echo "1. * [x] ["$i"](http://crazy-mouse.cn/Codes/Java/UniversityCourse/src/pers/crazymouse/exercises/"$i".java)"
+        echo $head "["$i"](http://crazy-mouse.cn/Codes/Java/UniversityCourse/src/pers/crazymouse/exercises/"$i".java)"
 done
 ) > README.md
