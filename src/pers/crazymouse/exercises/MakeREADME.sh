475e557a2d0d17256e24c7fcfee00a87006d57d6
crazymouse <crazymousethief@outlook.com>
2016-06-03 19:50:39 +0800

    Update "MakeREADME.sh".

--- src/pers/crazymouse/exercises/MakeREADME.sh
+++ src/pers/crazymouse/exercises/MakeREADME.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
-head="1. [x]"
+#head="1. [x]"                                                                                                                                                                                              
+head="1. "
 (echo "# [Java Exercises](http://crazy-mouse.cn/Codes/Java/UniversityCourse/src/pers/crazymouse/exercises/README.html)"
 echo ""
 echo "## myclass"
 ls myclass/*.java| cut -d . -f 1 | while read i
 do
-        echo $head "["${i##*/}"](http://crazy-mouse.cn/Codes/Java/UniversityCourse/src/pers/crazymouse/exercises/"$i".java)"
+        echo $head"["${i##*/}"](http://crazy-mouse.cn/Codes/Java/UniversityCourse/src/pers/crazymouse/exercises/"$i".java)"
 done
 echo ""
 echo "## myinterface"
 ls myinterface/*.java| cut -d . -f 1 | while read i
 do
-        echo $head "["${i##*/}"](http://crazy-mouse.cn/Codes/Java/UniversityCourse/src/pers/crazymouse/exercises/"$i".java)"
+        echo $head"["${i##*/}"](http://crazy-mouse.cn/Codes/Java/UniversityCourse/src/pers/crazymouse/exercises/"$i".java)"
 done
 echo ""
 echo "## exercises"
 ls *.java| cut -d . -f 1 | sort -n -t "_" -k 1.3 -k 2 | while read i
 do
-        echo $head "["$i"](http://crazy-mouse.cn/Codes/Java/UniversityCourse/src/pers/crazymouse/exercises/"$i".java)"
+        echo $head"["$i"](http://crazy-mouse.cn/Codes/Java/UniversityCourse/src/pers/crazymouse/exercises/"$i".java)"
 done
 ) > README.md
